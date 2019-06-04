3f0a27716bde2c2804e62a5f97f75e79fc33e469
Peter Christensen <peter.f.chris@gmail.com>
2019-06-04 09:53:29 -0600

    first commit

---
+++ server/controllers/controller.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+    register: async (req, res) => {
+        const {firstname, lastname, email, username, password} = req.body
+        const db = req.app.get('db')
+        const {session} = req
+        const userFound = await db.check_user_email({email})
+        if (userFound[0]) return res.status(409).send('Email already exists')
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt)
+        const createdUser = await db.register_user({
+            firstname,
+            lastname,
+            email,
+            username,
+            password: hash
+        })
+        session.user = {id: createdUser[0].login_id, username: createdUser[0].username}
+        res.status(200).send(session.user)
+    },
+    login: async (req, res) => {
+        const db = req.app.get('db')
+        const {session} = req
+        if(session.user) {
+            const details = await db.get_user_details({id: session.user.id})
+            const {firstname, email, user_id} = details[0]
+            return res.status(200).send({
+                firstname,
+                email,
+                user_id,
+                username: session.user.username
+            })
+        }
+        return res.status(401).send(`Please Log In`)
+    },
+    getUser: (req, res) => {
+        if(req.session.user) return res.status(200).send(session.user)
+    },
+    logout: (req, res) => {
+        req.session.destroy()
+        res.sendStatus(200)
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express'),
+        session = require('express-session'),
+        massive = require('massive'),
+        ctrl = require('./controllers/controller')
+const app = express()
+const {CONNECTION_STRING, SERVER_PORT, SESSION_SECRET} = process.env
+
+app.use(express.json())
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        saveUninitialized: false,
+        resave: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60
+        }
+    })
+)
+
+massive(CONNECTION_STRING).then((database) => {
+    app.set('db', database)
+    console.log('database set!', database.listTables())
+    app.listen(SERVER_PORT, () => console.log(`Port ${SERVER_PORT} is alive and well`))
+})
+
+// Endpoints
