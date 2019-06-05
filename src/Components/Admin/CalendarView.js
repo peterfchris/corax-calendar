55d340964b3616e3e8d65b9cec78dc7f920356b3
Peter Christensen <peter.f.chris@gmail.com>
2019-06-05 16:27:03 -0600

    calendar wrapper added

--- src/Components/Admin/CalendarView.js
+++ src/Components/Admin/CalendarView.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
+import FullCalendar from 'fullcalendar-reactwrapper'
 import {Link} from 'react-router-dom'
 
+import "fullcalendar-reactwrapper/dist/css/fullcalendar.min.css"; 
+
 export class CalendarView extends Component {
+    constructor(){
+        super()
+        this.state = {
+            events: []
+        }
+    }
     render() {
         return (
             <div>
                 <h1>Calendar View</h1>
                 <button><Link to='/new-event'>Create Event</Link></button>
+                <FullCalendar 
+                    id="user-calendar"
+                    header = {{
+                        left: 'prev,next today myCustomButton',
+                        center: 'title',
+                        right: 'month,basicWeek,basicDay'
+                    }}
+                    navLinks= {true} 
+                    editable= {true}
+                    events = {this.state.events}
+                />
             </div>
         )
     }
--- src/Components/User/UserCalendar.js
+++ src/Components/User/UserCalendar.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import FullCalendar from 'fullcalendar-reactwrapper'
 import {Link} from 'react-router-dom'
 
+import "fullcalendar-reactwrapper/dist/css/fullcalendar.min.css"; 
+
 export class UserCalendar extends Component {
+    constructor(){
+        super()
+        this.state = {
+            events: []
+        }
+    }
     render() {
         return (
             <div>
                 <h1><Link to='/confirmation'>User Calendar</Link></h1>
+                <FullCalendar 
+                    id="user-calendar"
+                    header = {{
+                        left: 'prev,next today myCustomButton',
+                        center: 'title',
+                        right: 'month,basicWeek,basicDay'
+                    }}
+                    navLinks= {true} 
+                    editable= {true}
+                    events = {this.state.events}
+                />
             </div>
         )
     }
