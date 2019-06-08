bcfa04a5c9e9486b0a4caf67d6f0e6831dc969ab
Peter Christensen <peter.f.chris@gmail.com>
2019-06-08 15:07:36 -0600

    styled home page

--- src/Components/User/Home.js
+++ src/Components/User/Home.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import './Home.css'
 
 export class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Corax Calendar</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br /> eiusmod 
-                   tempor incididunt ut labore et dolore magna aliqua.</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco <br /> laboris nisi ut 
-                   aliquip ex ea commodo consequat.</p>
-                <button><Link to='/lead-form'>Schedule a Free Consultation</Link></button>
+                <div className='home-page'>
+                    <div className='home-container'>
+                        <h1 className='home-header'>Corax Calendar</h1>
+                        <p className='text' >Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                        et dolore magna aliqua.</p>
+                        <p className='text'>Ut enim ad minim veniam, quis nostrud 
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                        consequat.</p>
+                        <button className='schedule-btn' >Schedule a Free Consultation</button>
+                    </div>
+                </div>
             </div>
         )
     }
